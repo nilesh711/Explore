79ad4af428aa3dff583e7b35251218027a14906f
nilesh711 <nileshrewasiya99@gmail.com>
2025-05-31 23:59:56 +0530

    Corrected export default mistakes

--- Backend/models/conversation.model.js
+++ Backend/models/conversation.model.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema({
-    participants: [{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
-    message: [{type:mongoose.Schema.Types.ObjectId, ref:'Message'}]
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  message: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
 });
 
-export default Conversation = mongoose.model('Conversation', conversationSchema);
+export const Conversation = mongoose.model("Conversation", conversationSchema);
