1967f3d88c542427262e91ccb2df51a47c248dc1
nilesh711 <nileshrewasiya99@gmail.com>
2025-06-01 16:04:49 +0530

    Formatted Document

--- Backend/server.js
+++ Backend/server.js
@@ -3,31 +3,31 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./utlis/db.js";
-import userRoute from "./router/user.router.js"
+import userRoute from "./router/user.router.js";
 
 dotenv.config({});
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(_, res)=>{
+app.get("/", (_, res) => {
     return res.status(200).json({
         message: "Checking server",
-        success: true
-    })
-})
+        success: true,
+    });
+});
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({extended:true}));
+app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true
-}
+    origin: "http://localhost:5173",
+    credentials: true,
+};
 app.use(cors(corsOptions));
 
 app.use("/api/v1/user", userRoute);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server is listening on port ${PORT}`);
-})
+});
